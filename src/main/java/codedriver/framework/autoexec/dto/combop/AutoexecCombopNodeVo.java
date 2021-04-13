bda3b3a28cd0cb31666109272bcf9228162a34ba
林邦泉 <linbq@techsure.com.cn>
2021-04-13 20:22:51 +0800

    --story=1004261 --user=林邦泉 自动化-后端-组合工具 https://www.tapd.cn/54247054/s/1150107

---
+++ src/main/java/codedriver/framework/autoexec/dto/combop/AutoexecCombopNodeVo.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright(c) 2021 TechSureCo.,Ltd.AllRightsReserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.framework.autoexec.dto.combop;
+
+/**
+ * @author: linbq
+ * @since: 2021/4/13 10:02
+ **/
+public class AutoexecCombopNodeVo {
+}
---
+++ src/main/java/codedriver/framework/autoexec/dto/combop/AutoexecCombopParamVo.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright(c) 2021 TechSureCo.,Ltd.AllRightsReserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.framework.autoexec.dto.combop;
+
+/**
+ * 组合工具全局参数Vo类
+ * @author: linbq
+ * @since: 2021/4/13 10:04
+ **/
+public class AutoexecCombopParamVo {
+    private Long combopId;
+    private String key;
+    private String value;
+    private String description;
+    private Integer isRequired;
+    private String type;
+    private String mode;
+
+    public Long getCombopId() {
+        return combopId;
+    }
+
+    public void setCombopId(Long combopId) {
+        this.combopId = combopId;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getIsRequired() {
+        return isRequired;
+    }
+
+    public void setIsRequired(Integer isRequired) {
+        this.isRequired = isRequired;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+}
