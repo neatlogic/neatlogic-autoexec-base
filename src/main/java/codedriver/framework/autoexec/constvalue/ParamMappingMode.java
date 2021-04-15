b1aa8c59cd3853468e40dbb6512e39e1aeb243c7
林邦泉 <linbq@techsure.com.cn>
2021-04-15 11:35:15 +0800

    --story=1004261 --user=林邦泉 自动化-后端-组合工具 https://www.tapd.cn/54247054/s/1150303

--- src/main/java/codedriver/framework/autoexec/constvalue/ParamMappingMode.java
+++ src/main/java/codedriver/framework/autoexec/constvalue/ParamMappingMode.java
@@ -5,19 +5,25 @@
 
 package codedriver.framework.autoexec.constvalue;
 
+import codedriver.framework.common.constvalue.IEnum;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.List;
+
 /**
  * 参数映射模式枚举类
  *
  * @author: linbq
  * @since: 2021/4/14 17:47
  **/
-public enum ParamMappingMode {
+public enum ParamMappingMode implements IEnum {
     CONSTANT("constant", "常量"),
     GLOBAL_PARAM("globalparam", "全局参数"),
     PRE_NODE_OUTPUT_PARAM("prenodeoutputparam", "上游节点输出参数");
 
     private String value;
     private String text;
+    private JSONObject config;
 
     private ParamMappingMode(String value, String text) {
         this.value = value;
@@ -30,5 +36,20 @@ public enum ParamMappingMode {
 
     public String getText() {
         return text;
+    }
+
+    public JSONObject getConfig(){
+        return config;
+    }
+    /**
+     * @Description: 不同的枚举类，返回不同的枚举值，可自由组合成List<>或者JSONArray
+     * @Author: laiwt
+     * @Date: 2021/1/12 14:57
+     * @Params: []
+     * @Returns: java.util.List
+     **/
+    @Override
+    public List getValueTextList() {
+        return null;
     }
 }
