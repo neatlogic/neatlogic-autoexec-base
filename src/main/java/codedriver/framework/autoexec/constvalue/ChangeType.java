6d0aef6ff8634b35ac1fb8506ddf30b15672d725
赖文韬 <laiwt@techsure.com.cn>
2021-04-22 12:48:00 +0800

    --task=1004561 --user=赖文韬 后端-脚本版本对比接口 https://www.tapd.cn/54247054/s/1151900

---
+++ src/main/java/codedriver/framework/autoexec/constvalue/ChangeType.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright(c) 2021. TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.framework.autoexec.constvalue;
+
+public enum ChangeType {
+    INSERT("insert", "插入"),
+    DELETE("delete", "删除"),
+    UPDATE("update", "更新");
+    private String value;
+    private String text;
+
+    private ChangeType(String value, String text) {
+        this.value = value;
+        this.text = text;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
