85d5c62067a71168ae5af8ec396522699dc9cdbb
赖文韬 <laiwt@techsure.com.cn>
2021-05-19 15:47:15 +0800

    --task=1004637 --user=赖文韬 导入自定义工具-接口 https://www.tapd.cn/54247054/s/1155531

--- src/main/java/codedriver/framework/autoexec/dto/script/AutoexecScriptVersionVo.java
+++ src/main/java/codedriver/framework/autoexec/dto/script/AutoexecScriptVersionVo.java
@@ -16,6 +16,7 @@ import com.alibaba.fastjson.annotation.JSONField;
 import org.apache.commons.collections4.CollectionUtils;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -137,6 +138,15 @@ public class AutoexecScriptVersionVo extends BaseEditorVo implements Serializabl
     }
 
     public List<AutoexecScriptVersionParamVo> getParamList() {
+        if (CollectionUtils.isNotEmpty(inputParamList) || CollectionUtils.isNotEmpty(outputParamList)) {
+            paramList = new ArrayList<>();
+            if (inputParamList != null) {
+                paramList.addAll(inputParamList);
+            }
+            if (outputParamList != null) {
+                paramList.addAll(outputParamList);
+            }
+        }
         return paramList;
     }
 
