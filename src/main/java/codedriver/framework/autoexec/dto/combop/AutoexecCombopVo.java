a752e77b9c0fb860534f03b51528a6d6ba317e86
林邦泉 <linbq@techsure.com.cn>
2021-05-25 17:45:53 +0800

    --task=1004741 --user=林邦泉 补充工具库相关逻辑 https://www.tapd.cn/54247054/s/1156384

--- src/main/java/codedriver/framework/autoexec/dto/combop/AutoexecCombopVo.java
+++ src/main/java/codedriver/framework/autoexec/dto/combop/AutoexecCombopVo.java
@@ -6,6 +6,7 @@
 package codedriver.framework.autoexec.dto.combop;
 
 import codedriver.framework.autoexec.constvalue.CombopOperationType;
+import codedriver.framework.autoexec.dto.AutoexecToolAndScriptVo;
 import codedriver.framework.autoexec.dto.AutoexecToolVo;
 import codedriver.framework.autoexec.dto.script.AutoexecScriptVo;
 import codedriver.framework.common.constvalue.ApiParamType;
@@ -71,18 +72,25 @@ public class AutoexecCombopVo extends BaseEditorVo implements Serializable {
     public AutoexecCombopVo() {
     }
 
-    public AutoexecCombopVo(AutoexecScriptVo autoexecScriptVo) {
-        this.uk = autoexecScriptVo.getUk();
-        this.name = autoexecScriptVo.getName();
-        this.typeId = autoexecScriptVo.getTypeId();
-        this.operationType = CombopOperationType.SCRIPT.getValue();
-    }
+//    public AutoexecCombopVo(AutoexecScriptVo autoexecScriptVo) {
+//        this.uk = autoexecScriptVo.getUk();
+//        this.name = autoexecScriptVo.getName();
+//        this.typeId = autoexecScriptVo.getTypeId();
+//        this.operationType = CombopOperationType.SCRIPT.getValue();
+//    }
+//
+//    public AutoexecCombopVo(AutoexecToolVo autoexecToolVo) {
+//        this.uk = autoexecToolVo.getUk();
+//        this.name = autoexecToolVo.getName();
+//        this.typeId = autoexecToolVo.getTypeId();
+//        this.operationType = CombopOperationType.TOOL.getValue();
+//    }
 
-    public AutoexecCombopVo(AutoexecToolVo autoexecToolVo) {
-        this.uk = autoexecToolVo.getUk();
-        this.name = autoexecToolVo.getName();
-        this.typeId = autoexecToolVo.getTypeId();
-        this.operationType = CombopOperationType.TOOL.getValue();
+    public AutoexecCombopVo(AutoexecToolAndScriptVo autoexecToolAndScriptVo) {
+        this.uk = autoexecToolAndScriptVo.getUk();
+        this.name = autoexecToolAndScriptVo.getName();
+        this.typeId = autoexecToolAndScriptVo.getTypeId();
+        this.operationType = autoexecToolAndScriptVo.getType();
     }
 
     public Long getId() {
