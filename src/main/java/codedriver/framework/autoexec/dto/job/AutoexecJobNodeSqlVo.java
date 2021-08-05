5314b0f0b7e0630e356a502341f4f0134a3969b2
吕佐康 <lvzk@techsure.com.cn>
2021-08-05 18:41:45 +0800

    --story=1005040 --user=lvzk 执行sql https://www.tapd.cn/54247054/s/1169200

--- src/main/java/codedriver/framework/autoexec/dto/job/AutoexecJobNodeSqlVo.java
+++ src/main/java/codedriver/framework/autoexec/dto/job/AutoexecJobNodeSqlVo.java
@@ -5,10 +5,12 @@
 
 package codedriver.framework.autoexec.dto.job;
 
+import codedriver.framework.autoexec.constvalue.JobNodeStatus;
 import codedriver.framework.common.constvalue.ApiParamType;
 import codedriver.framework.restful.annotation.EntityField;
 import codedriver.framework.util.TimeUtil;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.Date;
 
@@ -21,6 +23,8 @@ public class AutoexecJobNodeSqlVo {
     private JSONObject interact;
     @EntityField(name = "sql执行状态", type = ApiParamType.STRING)
     private String status;
+    @EntityField(name = "sql执行状态名", type = ApiParamType.STRING)
+    private String statusName;
     @EntityField(name = "执行开始时间", type = ApiParamType.LONG)
     private Date startTime;
     @EntityField(name = "执行结束时间", type = ApiParamType.LONG)
@@ -54,6 +58,13 @@ public class AutoexecJobNodeSqlVo {
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    public String getStatusName() {
+        if(StringUtils.isNotBlank(status)){
+            return JobNodeStatus.getText(status);
+        }
+        return statusName;
     }
 
     public Date getStartTime() {
