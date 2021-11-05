30dc927499d5e16d75f27be1ec76c8719ae5cdf2
吕佐康 <lvzk@techsure.com.cn>
2021-11-05 19:50:49 +0800

    fixed 重复重跑逻辑

--- src/main/java/codedriver/framework/autoexec/exception/AutoexecJobCanNotRefireException.java
+++ src/main/java/codedriver/framework/autoexec/exception/AutoexecJobCanNotRefireException.java
@@ -12,7 +12,7 @@ public class AutoexecJobCanNotRefireException extends ApiRuntimeException {
     private static final long serialVersionUID = -7079932723076796784L;
 
     public AutoexecJobCanNotRefireException(String jobId) {
-        super("无法重跑作业: "+jobId+"，请确保作业以及阶段不处于'运行中（running）'状态");
+        super("无法重跑作业: "+jobId+"，请确保作业不处于'运行中'、'中止中'、'待运行'状态");
     }
 
 
