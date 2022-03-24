8d251461a38c252554f7ad2acd74d414250802bf
赖文韬 <laiwt@techsure.com.cn>
2022-03-24 11:23:11 +0800

    --story=1006078 --user=赖文韬 自动化组合工具作业，增加保存作业操作 https://www.tapd.cn/54247054/s/1216664

---
+++ src/main/java/codedriver/framework/autoexec/exception/AutoexecJobCanNotRevokeException.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright(c) 2021. TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
+package codedriver.framework.autoexec.exception;
+
+import codedriver.framework.exception.core.ApiRuntimeException;
+
+public class AutoexecJobCanNotRevokeException extends ApiRuntimeException {
+
+    private static final long serialVersionUID = -6343241416891736903L;
+
+    public AutoexecJobCanNotRevokeException(Long jobId) {
+        super("无法撤销作业: " + jobId + "，请确保作业处于'已就绪（ready）'状态");
+    }
+
+
+}
