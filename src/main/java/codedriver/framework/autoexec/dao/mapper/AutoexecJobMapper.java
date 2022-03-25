6c8b42a17ada4bbecc5a729f6eb14b629a7befb3
赖文韬 <laiwt@techsure.com.cn>
2022-03-25 12:04:30 +0800

    --story=1006078 --user=赖文韬 自动化组合工具作业，增加保存作业操作 https://www.tapd.cn/54247054/s/1216935

--- src/main/java/codedriver/framework/autoexec/dao/mapper/AutoexecJobMapper.java
+++ src/main/java/codedriver/framework/autoexec/dao/mapper/AutoexecJobMapper.java
@@ -45,7 +45,7 @@ public interface AutoexecJobMapper {
 
     AutoexecJobVo getJobLockByOperationId(Long operationId);
 
-    List<Long> getJobIdListByStatus(String status);
+    List<Long> getJobIdListByStatusAndTriggerType(@Param("status") String status, @Param("triggerType") String triggerType);
 
     //jobPhase
     List<AutoexecJobPhaseVo> getJobPhaseListByJobId(Long jobId);
@@ -221,7 +221,7 @@ public interface AutoexecJobMapper {
 
     Integer updateJobPhaseNodeFrom(@Param("jobPhaseId") Long id, @Param("nodeFrom") String nodeFrom);
 
-    Integer updateJobPhaseNodeResetStartTimeAndEndTimeByNodeIdList(@Param("nodeIdList")List<Long> nodeIdList);
+    Integer updateJobPhaseNodeResetStartTimeAndEndTimeByNodeIdList(@Param("nodeIdList") List<Long> nodeIdList);
 
     int insertDuplicateJobEnv(AutoexecJobEnvVo jobEnvVo);
 
