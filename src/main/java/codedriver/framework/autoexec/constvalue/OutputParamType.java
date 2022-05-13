700fbb4bfa42fe05e924ce9129369da89fec58cb
linbq@techsure.com.cn <Besiter789>
2022-05-13 10:50:53 +0800

    --task=1006310 --user=林邦泉 后端-自定义工具库出参类型补充和入参一致 https://www.tapd.cn/54247054/s/1227490

--- src/main/java/codedriver/framework/autoexec/constvalue/OutputParamType.java
+++ src/main/java/codedriver/framework/autoexec/constvalue/OutputParamType.java
@@ -21,6 +21,18 @@ import java.util.Objects;
  **/
 public enum OutputParamType implements IEnum {
     TEXT("text", "文本", "可输出字符串、数字"),
+    PASSWORD("password", "密码",  "可输出数字或字符串，页面显示为密文"),
+    FILE("file", "文件",  "支持多个文件同时上传，执行时，自动上传文件到目标主机特定目录下，保留原文件名"),
+    DATE("date", "日期",  "日期选择器"),
+    DATETIME("datetime", "日期时间", "日期时间选择器"),
+    TIME("time", "时间", "时间选择器"),
+    JSON("json", "json对象","支持json对象和json数组，输出内容需是合法Json格式"),
+    SELECT("select", "单选下拉", "单选下拉选择器"),
+    MULTISELECT("multiselect", "多选下拉", "多选下拉选择器"),
+    RADIO("radio", "单选", "单选选择器"),
+    CHECKBOX("checkbox", "复选", "复选选择器"),
+    NODE("node", "节点信息", "将节点的相关信息生成脚本变量，但不会分派到该节点执行"),
+    ACCOUNT("account", "账号", "服务的连接协议、帐号，用户连接主机上的数据库，中间件等服务"),
     FILEPATH("filepath", "文件路径",  "可输出文件路径参数");
 
     private final String value;
