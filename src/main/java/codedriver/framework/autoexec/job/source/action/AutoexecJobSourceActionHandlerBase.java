99e9b080941062342e9673b4dc78efc077e30d0c
longrf <a5739119>
2022-06-01 12:05:30 +0800

    --story=1006396 --user=龙儒锋 自动化-作业详情-sql文件下载 https://www.tapd.cn/54247054/s/1232735

--- src/main/java/codedriver/framework/autoexec/job/source/action/AutoexecJobSourceActionHandlerBase.java
+++ src/main/java/codedriver/framework/autoexec/job/source/action/AutoexecJobSourceActionHandlerBase.java
@@ -1,11 +1,5 @@
 package codedriver.framework.autoexec.job.source.action;
 
-import codedriver.framework.autoexec.exception.AutoexecJobRunnerConnectRefusedException;
-import codedriver.framework.autoexec.exception.AutoexecJobRunnerHttpRequestException;
-import codedriver.framework.dto.RestVo;
-import codedriver.framework.integration.authentication.enums.AuthenticateType;
-import codedriver.framework.util.RestUtil;
-import com.alibaba.fastjson.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,26 +11,4 @@ public abstract class AutoexecJobSourceActionHandlerBase implements IAutoexecJob
 
     Logger logger = LoggerFactory.getLogger(AutoexecJobSourceActionHandlerBase.class);
 
-    /**
-     * 请求runner
-     *
-     * @param runnerUrl runner 链接
-     * @param paramJson 入参
-     * @return runner response
-     */
-    protected String requestRunner(String runnerUrl, JSONObject paramJson) {
-        RestVo restVo = new RestVo.Builder(runnerUrl, AuthenticateType.BUILDIN.getValue()).setPayload(paramJson).build();
-        String restResult = RestUtil.sendPostRequest(restVo);
-        JSONObject resultJson = null;
-        try {
-            resultJson = JSONObject.parseObject(restResult);
-        } catch (Exception ex) {
-            logger.error(ex.getMessage(), ex);
-            throw new AutoexecJobRunnerConnectRefusedException(restVo.getUrl() + " " + restResult);
-        }
-        if (!resultJson.containsKey("Status") || !"OK".equals(resultJson.getString("Status"))) {
-            throw new AutoexecJobRunnerHttpRequestException(resultJson.getString("Message"));
-        }
-        return resultJson.getString("Return");
-    }
 }
