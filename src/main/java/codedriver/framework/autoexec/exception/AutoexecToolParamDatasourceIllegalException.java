7356721d1d1dc86a75f19e7f6bb76c24e766e204
赖文韬 <laiwt@techsure.com.cn>
2022-06-10 11:14:07 +0800

    --story=1006440 --user=赖文韬 注册自定义工具库 https://www.tapd.cn/54247054/s/1234442

--- src/main/java/codedriver/framework/autoexec/exception/AutoexecToolParamDatasourceIllegalException.java
+++ src/main/java/codedriver/framework/autoexec/exception/AutoexecToolParamDatasourceIllegalException.java
@@ -12,7 +12,7 @@ public class AutoexecToolParamDatasourceIllegalException extends ApiRuntimeExcep
     private static final long serialVersionUID = -7415182960002285047L;
 
     public AutoexecToolParamDatasourceIllegalException(String param) {
-        super("参数：'" + param + "'的数据源格式不正确，正确格式为：[{\"text\":\"是\",\"value\":\"1\"},{\"text\":\"否\",\"value\":\"0\"}]，其中(text的key和value)与(value的key)不能为空");
+        super("参数：'" + param + "'的数据源格式不正确，正确格式为：{\"dataList\":[{\"text\":\"是\",\"value\":\"1\"},{\"text\":\"否\",\"value\":\"0\"}]}，其中(text的key和value)与(value的key)不能为空");
     }
 
 
