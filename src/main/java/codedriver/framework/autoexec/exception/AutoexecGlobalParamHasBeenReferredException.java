6d798166f36932d607a21239d5d73af4727db982
longrf <a5739119>
2022-06-14 11:26:44 +0800

    profile数据结构优化

--- src/main/java/codedriver/framework/autoexec/exception/AutoexecGlobalParamHasBeenReferredException.java
+++ src/main/java/codedriver/framework/autoexec/exception/AutoexecGlobalParamHasBeenReferredException.java
@@ -2,14 +2,12 @@ package codedriver.framework.autoexec.exception;
 
 import codedriver.framework.exception.core.ApiRuntimeException;
 
-import java.util.Objects;
-
 /**
  * @author longrf
  * @date 2022/6/10 3:05 下午
  */
 public class AutoexecGlobalParamHasBeenReferredException extends ApiRuntimeException {
     public AutoexecGlobalParamHasBeenReferredException(String name) {
-        super("全局参数：被" + (Objects.equals(name, "profile") ? "预制参数集" : "组合工具") + " 引用,无法删除");
+        super("全局参数“" + name + "” 被引用,无法删除");
     }
 }
