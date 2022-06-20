0cf21a7f0811c4770d9aa8019f957e70fafddff0
linbq@techsure.com.cn <Besiter789>
2022-06-20 18:04:40 +0800

    --task=1006490 --user=林邦泉 后端-应用配置-流水线作业参数保存与获取接口 https://www.tapd.cn/54247054/s/1236466

--- src/main/java/codedriver/framework/autoexec/crossover/IAutoexecServiceCrossoverService.java
+++ src/main/java/codedriver/framework/autoexec/crossover/IAutoexecServiceCrossoverService.java
@@ -4,6 +4,8 @@ import codedriver.framework.autoexec.dto.AutoexecParamVo;
 import codedriver.framework.autoexec.dto.combop.AutoexecCombopConfigVo;
 import codedriver.framework.crossover.ICrossoverService;
 
+import java.util.List;
+
 /**
  * @author longrf
  * @date 2022/1/24 6:33 下午
@@ -13,4 +15,6 @@ public interface IAutoexecServiceCrossoverService extends ICrossoverService {
     void updateAutoexecCombopConfig(AutoexecCombopConfigVo config);
 
     void mergeConfig(AutoexecParamVo autoexecParamVo);
+
+    void validateRuntimeParamList(List<? extends AutoexecParamVo> runtimeParamList);
 }
