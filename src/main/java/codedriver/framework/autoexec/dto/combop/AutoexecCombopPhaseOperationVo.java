a6a2d475306027f670be656a5151c767a11596d0
linbq@techsure.com.cn <Besiter789>
2022-07-20 10:09:47 +0800

    --task=1006584 --user=林邦泉 后端-【运维自动化】-组合工具 operation 存储逻辑优化 https://www.tapd.cn/54247054/s/1241830

--- src/main/java/codedriver/framework/autoexec/dto/combop/AutoexecCombopPhaseOperationVo.java
+++ src/main/java/codedriver/framework/autoexec/dto/combop/AutoexecCombopPhaseOperationVo.java
@@ -77,6 +77,9 @@ public class AutoexecCombopPhaseOperationVo implements Serializable {
     }
 
     public String getOperationName() {
+        if (operation != null) {
+            return operation.getName();
+        }
         return operationName;
     }
 
