c3c23d561a5d51f9c8712ed6b689ec3822a76db9
赖文韬 <laiwt@techsure.com.cn>
2022-10-21 11:46:47 +0800

    --story=1007026 --user=赖文韬 自动化/自动发布-自定义工具/工具库-支持绑定【自定义模板】 https://www.tapd.cn/54247054/s/1257815

--- src/main/java/codedriver/framework/autoexec/constvalue/AutoexecFromType.java
+++ src/main/java/codedriver/framework/autoexec/constvalue/AutoexecFromType.java
@@ -7,13 +7,14 @@ import codedriver.framework.dependency.core.IFromType;
  * @date 2021/12/16 4:16 下午
  */
 public enum AutoexecFromType implements IFromType {
-    AUTOEXEC_CATALOG("autoexeccatalog","自动化工具目录"),
-    AUTOEXEC_PROFILE_OPERATION("autoexecprofileoperation","profile自动化工具库工具和自定义工具"),
-    PROFILE("profile","预置参数集"),
-    SCRIPT("script","自定义工具"),
-    TOOL("tool","工具"),
-    GLOBAL_PARAM("globalparam","全局参数"),
-    SCENARIO("scenario","场景")
+    AUTOEXEC_CATALOG("autoexeccatalog", "自动化工具目录"),
+    AUTOEXEC_PROFILE_OPERATION("autoexecprofileoperation", "profile自动化工具库工具和自定义工具"),
+    PROFILE("profile", "预置参数集"),
+    SCRIPT("script", "自定义工具"),
+    TOOL("tool", "工具"),
+    GLOBAL_PARAM("globalparam", "全局参数"),
+    SCENARIO("scenario", "场景"),
+    CUSTOM_TEMPLATE("customtemplate", "自定义模版"),
     ;
 
     private String value;
