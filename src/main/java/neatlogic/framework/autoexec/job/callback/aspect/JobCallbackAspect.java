44230bc56d043a3cff3a995ba4345d630964e447
chenqw <zenkiray@live.com>
2023-06-28 18:32:40 +0800

    - [修复]优化任务变更记录   - 通过AOP截获修改操作生成变更记录。
    Signed-off-by: chenqw <zenkiray@live.com>

--- src/main/java/neatlogic/framework/autoexec/job/callback/aspect/JobCallbackAspect.java
+++ src/main/java/neatlogic/framework/autoexec/job/callback/aspect/JobCallbackAspect.java
@@ -1,17 +1,17 @@
 /*
-Copyright(c) 2023 NeatLogic Co., Ltd. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+ * Copyright(c) 2023 NeatLogic Co., Ltd. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package neatlogic.framework.autoexec.job.callback.aspect;
@@ -71,7 +71,7 @@ public class JobCallbackAspect {
             Object param = params[i];
             Annotation[] paramAnn = annotations[i];
             // 参数为空或没有注解，直接检查下一个参数
-            if (param == null || paramAnn == null || paramAnn.length == 0) {
+            if (param == null || paramAnn == null) {
                 continue;
             }
             for (Annotation annotation : paramAnn) {
