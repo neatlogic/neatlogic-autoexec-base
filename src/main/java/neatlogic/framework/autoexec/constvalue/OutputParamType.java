a84d4cd867a677bb71cc69e8b9e51657b756a660
chenqw <zenkiray@live.com>
2023-07-28 10:44:55 +0800

    - [修复]响应国家号召修改错别字
    Signed-off-by: chenqw <zenkiray@live.com>

--- src/main/java/neatlogic/framework/autoexec/constvalue/OutputParamType.java
+++ src/main/java/neatlogic/framework/autoexec/constvalue/OutputParamType.java
@@ -1,17 +1,17 @@
 /*
-Copyright(c) 2023 NeatLogic Co., Ltd. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+ * Copyright(c) 2023 NeatLogic Co., Ltd. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package neatlogic.framework.autoexec.constvalue;
@@ -44,7 +44,7 @@ public enum OutputParamType implements IEnum {
     RADIO("radio", "单选", "单选选择器"),
     CHECKBOX("checkbox", "复选框", "复选选择器"),
     NODE("node", "节点信息", "将节点的相关信息生成脚本变量，但不会分派到该节点执行"),
-    ACCOUNT("account", "帐号", "common.autoexec.account"),
+    ACCOUNT("account", "账号", "common.autoexec.account"),
     FILEPATH("filepath", "文件路径", "可输出文件路径参数"),
     TEXTAREA("textarea", "文本域", "可输出字符串、数字"),
     PHASE("phase", "阶段", "可输出阶段");
--- src/main/java/neatlogic/framework/autoexec/constvalue/ParamType.java
+++ src/main/java/neatlogic/framework/autoexec/constvalue/ParamType.java
@@ -1,17 +1,17 @@
 /*
-Copyright(c) 2023 NeatLogic Co., Ltd. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+ * Copyright(c) 2023 NeatLogic Co., Ltd. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package neatlogic.framework.autoexec.constvalue;
@@ -44,7 +44,7 @@ public enum ParamType implements IEnum {
     RADIO("radio", "单选", "单选选择器"),
     CHECKBOX("checkbox", "复选", "复选选择器"),
     NODE("node", "节点信息", "将节点的相关信息生成脚本变量，但不会分派到该节点执行"),
-    ACCOUNT("account", "帐号", "common.autoexec.account"),
+    ACCOUNT("account", "账号", "common.autoexec.account"),
     USERSELECT("userselect", "用户选择器", "用于选择系统用户、分组、角色"),
     TEXTAREA("textarea", "文本域", "可输入字符串、数字"),
     PHASE("phase", "阶段", "可选择阶段"),
