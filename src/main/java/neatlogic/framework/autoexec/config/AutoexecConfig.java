57a586d399b6a06944df6c80664f4951f364594a
lvzk <897706680@qq.com>
2025-01-09 11:46:58 +0800

    [功能]autoexec.token 配置取消默认值必须定义，防止泄漏导致安全问题

---
+++ src/main/java/neatlogic/framework/autoexec/config/AutoexecConfig.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2025  深圳极向量科技有限公司 All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package neatlogic.framework.autoexec.config;
+
+import neatlogic.framework.common.config.IConfigListener;
+import neatlogic.framework.util.I18nUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class AutoexecConfig implements IConfigListener {
+    private static final Logger logger = LoggerFactory.getLogger(AutoexecConfig.class);
+    private static String PROXY_BASIC_USER_NAME;
+    private static String PROXY_BASIC_PASSWORD;
+    private static Boolean AUTOEXEC_JOB_IS_ALLOWED_MANUAL_TRIGGER_IN_ADVANCE; // 是否允许人工提前触发自动化作业
+    private static String AUTOEXEC_TOKEN;// autoexec用户的token
+
+    public static String PROXY_BASIC_USER_NAME() {
+        return PROXY_BASIC_USER_NAME;
+    }
+
+    public static String PROXY_BASIC_PASSWORD() {
+        return PROXY_BASIC_PASSWORD;
+    }
+
+    public static Boolean AUTOEXEC_JOB_IS_ALLOWED_MANUAL_TRIGGER_IN_ADVANCE() {
+        return AUTOEXEC_JOB_IS_ALLOWED_MANUAL_TRIGGER_IN_ADVANCE;
+    }
+    public static String AUTOEXEC_TOKEN() {
+        return AUTOEXEC_TOKEN;
+    }
+
+    @Override
+    public void loadConfig(Properties prop) {
+        PROXY_BASIC_USER_NAME = prop.getProperty("proxy.basic.username", "neatlogic");
+        PROXY_BASIC_PASSWORD = prop.getProperty("proxy.basic.password", "123456");
+        AUTOEXEC_JOB_IS_ALLOWED_MANUAL_TRIGGER_IN_ADVANCE = Boolean.valueOf(prop.getProperty("autoexec.job.isallowed.manualtrigger.inadvance", "true"));
+        AUTOEXEC_TOKEN = prop.getProperty("autoexec.token");
+        if(StringUtils.isBlank(AUTOEXEC_TOKEN)){
+            System.out.println(I18nUtils.getStaticMessage("nmac.autoexecconfig.loadconfig.autoexectoken"));
+            logger.error(I18nUtils.getStaticMessage("nmac.autoexecconfig.loadconfig.autoexectoken"));
+            System.exit(1);
+        }
+    }
+}
---
+++ src/main/java/neatlogic/framework/autoexec/constvalue/SystemUser.java
@@ -0,0 +1,140 @@
+/*Copyright (C) 2024  深圳极向量科技有限公司 All Rights Reserved.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+
+package neatlogic.framework.autoexec.constvalue;
+
+import neatlogic.framework.asynchronization.threadlocal.TenantContext;
+import neatlogic.framework.autoexec.config.AutoexecConfig;
+import neatlogic.framework.dto.AuthenticationInfoVo;
+import neatlogic.framework.dto.JwtVo;
+import neatlogic.framework.dto.UserVo;
+import neatlogic.framework.filter.core.LoginAuthHandlerBase;
+import neatlogic.framework.util.$;
+import neatlogic.framework.util.I18n;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
+
+public enum SystemUser {
+    AUTOEXEC("autoexec", "autoexec", new I18n("自动化用户"));
+
+    private final Logger logger = LoggerFactory.getLogger(SystemUser.class);
+
+    private final String userId;
+    private final String userUuid;
+    private final I18n userName;
+    private final String timezone = "+8:00";
+    private final AuthenticationInfoVo authenticationInfoVo;
+
+    SystemUser(String userId, String userUuid, I18n userName) {
+        this.userId = userId;
+        this.userUuid = userUuid;
+        this.userName = userName;
+        this.authenticationInfoVo = new AuthenticationInfoVo(userUuid);
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUserUuid() {
+        return userUuid;
+    }
+
+    public String getUserName() {
+        return $.t(userName.toString());
+    }
+
+    public String getTimezone() {
+        return timezone;
+    }
+
+    public String getToken() {
+        if (Objects.equals(userId, AUTOEXEC.getUserId())) {
+            return AutoexecConfig.AUTOEXEC_TOKEN();
+        }
+        return null;
+    }
+
+    public UserVo getUserVo() {
+        UserVo userVo = new UserVo();
+        userVo.setUuid(userUuid);
+        userVo.setUserId(userId);
+        userVo.setUserName(getUserName());
+        userVo.setTenant(TenantContext.get() != null ? TenantContext.get().getTenantUuid() : null);
+        userVo.setIsDelete(0);
+        userVo.setIsActive(1);
+        try {
+            JwtVo jwtVo = LoginAuthHandlerBase.buildJwt(userVo);
+            String authorization = "Bearer_" + jwtVo.getJwthead() + "." + jwtVo.getJwtbody() + "." + jwtVo.getJwtsign();
+            userVo.setAuthorization(authorization);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+        return userVo;
+    }
+
+    public UserVo getUserVo(Boolean isNeedJwt) {
+        UserVo userVo = new UserVo();
+        userVo.setUuid(userUuid);
+        userVo.setUserId(userId);
+        userVo.setUserName(getUserName());
+        userVo.setTenant(TenantContext.get() != null ? TenantContext.get().getTenantUuid() : null);
+        userVo.setIsDelete(0);
+        userVo.setIsActive(1);
+        if (isNeedJwt) {
+            try {
+                JwtVo jwtVo = LoginAuthHandlerBase.buildJwt(userVo);
+                String authorization = "Bearer_" + jwtVo.getJwthead() + "." + jwtVo.getJwtbody() + "." + jwtVo.getJwtsign();
+                userVo.setAuthorization(authorization);
+            } catch (Exception e) {
+                logger.error(e.getMessage(), e);
+            }
+        }
+        return userVo;
+    }
+
+    public AuthenticationInfoVo getAuthenticationInfoVo() {
+        return authenticationInfoVo;
+    }
+
+    public static String getUserName(String userUuid) {
+        for (SystemUser user : values()) {
+            if (user.getUserUuid().equals(userUuid)) {
+                return user.getUserName();
+            }
+        }
+        return "";
+    }
+
+    public static UserVo getUserVoByUser(String user) {
+        for (SystemUser systemUser : values()) {
+            if (systemUser.getUserUuid().equals(user) || systemUser.getUserId().equals(user)) {
+                return systemUser.getUserVo();
+            }
+        }
+        return null;
+    }
+
+    public static String getUserTokenByUser(String user) {
+        for (SystemUser systemUser : values()) {
+            if (systemUser.getUserUuid().equals(user) || systemUser.getUserId().equals(user)) {
+                return systemUser.getToken();
+            }
+        }
+        return null;
+    }
+}
