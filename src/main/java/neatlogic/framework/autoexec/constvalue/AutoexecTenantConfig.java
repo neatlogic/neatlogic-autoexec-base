e0e91eff037306402f564e5ea308223d6989fbd9
lvzk <897706680@qq.com>
2026-06-02 19:47:27 +0800

    [功能]新增 execrtool执行入口

--- src/main/java/neatlogic/framework/autoexec/constvalue/AutoexecTenantConfig.java
+++ src/main/java/neatlogic/framework/autoexec/constvalue/AutoexecTenantConfig.java
@@ -15,10 +15,15 @@ package neatlogic.framework.autoexec.constvalue;
 import neatlogic.framework.config.ITenantConfig;
 import neatlogic.framework.util.$;
 
+/**
+ * Tenant-level configuration keys used by autoexec features.
+ */
 public enum AutoexecTenantConfig implements ITenantConfig {
     MAX_NUM_OF_COMBOP_VERSION("maxNumOfCombopVersion", "10", "nfac.autoexectenantconfig.maxnumofcombopversion"),
     AUTOEXEC_JOB_LOG_ENCODING("autoexec.job.log.encoding", null, "nfac.autoexectenantconfig.autoexecjoblogencoding"),
-    AUTOEXEC_JOB_PARAM_VALID("autoexec.job.param.valid", "1", "启用作业参数校验,默认启动校验");
+    AUTOEXEC_JOB_PARAM_VALID("autoexec.job.param.valid", "1", "启用作业参数校验,默认启动校验"),
+    // 只控制 execrtool 同步调用 runner 的等待窗口，不影响正式作业 runner 请求。
+    AUTOEXEC_EXECRTOOL_RUNNER_READ_TIMEOUT("autoexec.execrtool.runner.read.timeout", "300000", "execrtool请求runner读取超时时间，单位毫秒，默认5分钟");
 
     final String key;
     final String value;
