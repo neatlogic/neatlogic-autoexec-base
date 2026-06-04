cba194299fc43fb94beda949e5b931989512c5dc
lvzk <897706680@qq.com>
2026-06-04 09:26:07 +0800

    [功能]新增 execrtool执行入口

---
+++ src/main/java/neatlogic/framework/autoexec/crossover/IAutoexecScriptExecrtoolCrossoverService.java
@@ -0,0 +1,23 @@
+/*
+ *
+ * Copyright (C) 2025  TechSure Co., Ltd.  All Rights Reserved.
+ * This file is part of the NeatLogic software.
+ * Licensed under the NeatLogic Sustainable Use License (NSUL), Version 4.x – 2025.
+ * You may use this file only in compliance with the License.
+ * See the LICENSE file distributed with this work for the full license text.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ */
+
+package neatlogic.framework.autoexec.crossover;
+
+import com.alibaba.fastjson.JSONObject;
+import neatlogic.framework.crossover.ICrossoverService;
+
+public interface IAutoexecScriptExecrtoolCrossoverService extends ICrossoverService {
+
+    Object execAutoexecScriptByTagent(JSONObject paramObj) throws Exception;
+
+    boolean checkAutoexecScriptHasExecrtoolAuthority(Long scriptId);
+}
