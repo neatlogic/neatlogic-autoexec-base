c481e4853c5581c9dfa6d0fee83700934efa82c6
lvzk <897706680@qq.com>
2026-08-10 19:24:56 +0800

    [功能]自动化skill优化

---
+++ src/main/java/neatlogic/framework/autoexec/constvalue/AutoexecOperationIndexAction.java
@@ -0,0 +1,17 @@
+/*
+ *
+ * Copyright (C) 2025  TechSure Co., Ltd.  All Rights Reserved.
+ * This file is part of the NeatLogic software.
+ * Licensed under the NeatLogic Sustainable Use License (NSUL), Version 4.x – 2025.
+ * You may use this file only in compliance with the License.
+ * See the LICENSE file in the project root for license information.
+ *
+ */
+
+package neatlogic.framework.autoexec.constvalue;
+
+/** Autoexec operation discovery index change action. */
+public enum AutoexecOperationIndexAction {
+    UPSERT,
+    DELETE
+}
