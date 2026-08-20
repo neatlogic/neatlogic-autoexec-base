d30102bfccbdc5e61f7a1cdae897afb77892db9c
lvzk <897706680@qq.com>
2026-08-20 17:20:32 +0800

    [功能]新增组合工具同步创建作业接口

--- src/main/java/neatlogic/framework/autoexec/constvalue/AutoexecTenantConfig.java
+++ src/main/java/neatlogic/framework/autoexec/constvalue/AutoexecTenantConfig.java
@@ -22,6 +22,9 @@ public enum AutoexecTenantConfig implements ITenantConfig {
     MAX_NUM_OF_COMBOP_VERSION("maxNumOfCombopVersion", "10", "nfac.autoexectenantconfig.maxnumofcombopversion"),
     AUTOEXEC_JOB_LOG_ENCODING("autoexec.job.log.encoding", null, "nfac.autoexectenantconfig.autoexecjoblogencoding"),
     AUTOEXEC_JOB_PARAM_VALID("autoexec.job.param.valid", "1", "nfacv.autoexectenantconfig.text.autoexec_job_param_valid"),
+    AUTOEXEC_JOB_SYNC_MAX_CONCURRENT("autoexec.job.sync.max.concurrent", "20", "nfacv.autoexectenantconfig.text.autoexec_job_sync_max_concurrent"),
+    AUTOEXEC_JOB_SYNC_OUTPUT_MAX_TARGET_COUNT("autoexec.job.sync.output.max.target.count", "10", "nfacv.autoexectenantconfig.text.autoexec_job_sync_output_max_target_count"),
+    AUTOEXEC_JOB_SYNC_WAIT_TIMEOUT("autoexec.job.sync.wait.timeout", "300000", "nfacv.autoexectenantconfig.text.autoexec_job_sync_wait_timeout"),
     // 只控制 execrtool 同步调用 runner 的等待窗口，不影响正式作业 runner 请求。
     AUTOEXEC_EXECRTOOL_RUNNER_READ_TIMEOUT("autoexec.execrtool.runner.read.timeout", "300000", "nfacv.autoexectenantconfig.text.autoexec_execrtool_runner_read_timeout");
 
